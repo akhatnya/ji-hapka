220ca29241769acc66affe5dec4aedac1eb1785b
verilog <z.kalkayev@jysanbank.kz>
2022-10-22 10:26:51 +0600

    Fix for ahat

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -14,7 +14,7 @@ const Header = () => {
                 </div>
             </div>
             <div className="col-center">
-                <a href="#" className="logotype">
+                <a href="/" className="logotype">
                     <svg viewBox="0 0 40 35">
                         <use href='/images/logo/logo.svg#logo'></use>
                     </svg>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../css/design.css';
-import '../css/responsive.css';
 import '../css/style.css';
+import '../css/responsive.css';
 import type { AppProps } from 'next/app'
 import { Header } from '../components/Header'
 import { Footer } from '../components/Footer'
@@ -8,7 +8,7 @@ import { Footer } from '../components/Footer'
 function MyApp({ Component, pageProps }: AppProps) {
   return (<>
         <Header />
-          <Component {...pageProps} />
+        <Component {...pageProps} />
         <Footer />
     </>)
 }
