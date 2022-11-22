66633bd19ca30bb4631b1d28203757d69d23a4a7
Ахат Молдабеков <akhatnya@gmail.com>
2022-11-22 23:41:51 +0600

    items with ar page created

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,12 +1,21 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
 import { Button } from "../components";
 
 const Banner = () => {
+  const router = useRouter();
+
   return (
     <div className="banner w-100-400">
         <div className="banner-text">
           <h1>Jihaz AR</h1>
           <p>Примерьте мебель<br/>у себя в комнате</p>
-          <Button iconRight={true} sizeIcon="28" svgIcon="/images/icons/AR-28.svg#root" title="Примерить мебель" className="btn btn-primary btn-54" />
+          <Button iconRight={true} 
+                    onClick={() => router.push("/ArItems")}
+                    sizeIcon="28" 
+                    svgIcon="/images/icons/AR-28.svg#root" 
+                    title="Примерить мебель" 
+                    className="btn btn-primary btn-54" />
         </div>
         <div className="banner-img" style={{ backgroundImage: "url(../images/banners/banner.png)"}}>
           <img src="images/banners/phone-hand.png" />
---
+++ pages/ArItems.tsx
@@ -0,0 +1,60 @@
+import { observer } from "mobx-react-lite";
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
+import { Card, Category, Breadcrumbs } from "../components";
+import { useBasketStore } from "../providers/RootStoreProvider";
+import { loadRooms, loadItemsByRoomId } from "../src/requests/requests";
+import { url } from "../src/urls";
+import { Title24 } from "../Typography";
+
+const ArItems = (props: any) => {
+    const store = useBasketStore();
+
+    const [items, setItems]: any = useState(props.items);
+
+    return (
+    <div className='page-wrapper'>
+        <div className="container">
+            <div className="row">
+                <div className="col-md-12">
+                    <Breadcrumbs className="pb-24 mb-24"/>
+
+                    <Title24 title="Примерьте в комнате" className="mb-32" />
+                    <div className="grid-max">
+                        {
+                            items?.map((i: any, index: any) => {
+                                return (
+                                            <span>
+                                                <Card 
+                                                    store={store}
+                                                    item={i}
+                                                    href={`/catalog/product/${i.item.id}`}
+                                                    index={index}
+                                                    title={i.item.nameRu} 
+                                                    backgroundImage={`url(${i.itemPhotos[0]?.photo.url}`}
+                                                    price={i.item.price}
+                                                    key={index}
+                                                    priceSale={i.item.price * 0.95}
+                                                />
+                                            </span>
+                                )
+                            })
+                        }
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+  );
+}
+
+export const getStaticProps = async (context: any) => {
+    return {
+        props: {
+            items: await (await fetch(url(`/items/with3d`))).json()
+        }
+    }
+}
+
+export default observer(ArItems);
