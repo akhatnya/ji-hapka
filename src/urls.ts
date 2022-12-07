e637d5d9b086ca4192b865f13200f932ce295258
Ахат Молдабеков <akhatnya@gmail.com>
2022-12-08 03:10:57 +0600

    url fixes

--- src/urls.ts
+++ src/urls.ts
@@ -1,4 +1,4 @@
-export const SERVER_BASE_URL = 'http://localhost:5085';
+export const SERVER_BASE_URL = 'http://46.101.212.100:3030';
 
 export const url = (urlPart: any) => {
     return `${SERVER_BASE_URL}${urlPart}`
